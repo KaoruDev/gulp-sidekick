d48e8d2a7c3cc825263e296c799bf9fead51c7a4
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-06-28 12:36:16 -0500

    Accept a different node.js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp'),
     spawn = require('child_process').spawn,
-    serverDir = require('minimist')(process.argv.slice(2)).serverDir,
+    argv = require('minimist')(process.argv.slice(2)),
+    serverDir = argv.serverDir,
+    nodeFile = argv.nodeFile || 'server.js',
     node
 ;
 
 gulp.task('server', function () {
-  if (node) { node.kill() };
-  console.log('\n Bouncing server..... \n');
-  node = spawn('node', ['../' + serverDir + '/server.js'], {stdio: 'inherit'});
+  if (node) {
+    node.kill()
+    console.log('\n Bouncing server..... \n');
+  };
+  node = spawn('node', ['../' + serverDir + '/' + nodeFile], {stdio: 'inherit'});
   node.on('close', function (code) {
     if (code === 8) {
       console.log('Error detected, waiting for changes...');
@@ -16,6 +20,8 @@ gulp.task('server', function () {
 });
 
 gulp.task('default', function () {
+  console.log("\n Starting up " + serverDir + '/' + nodeFile);
+  gulp.run('server');
   gulp.watch('../' + serverDir + '/*.js', ['server'])
 });
 
