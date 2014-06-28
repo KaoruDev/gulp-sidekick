73df13265c3c01153c39c1936b6eafe3a3cc5fc9
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-06-28 12:26:14 -0500

    successfully watch sibling directory for changes and run node server from gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    spawn = require('child_process').spawn,
+    serverDir = require('minimist')(process.argv.slice(2)).serverDir,
+    node
+;
+
+gulp.task('server', function () {
+  if (node) { node.kill() };
+  console.log('\n Bouncing server..... \n');
+  node = spawn('node', ['../' + serverDir + '/server.js'], {stdio: 'inherit'});
+  node.on('close', function (code) {
+    if (code === 8) {
+      console.log('Error detected, waiting for changes...');
+    }
+  });
+});
+
+gulp.task('default', function () {
+  gulp.watch('../' + serverDir + '/*.js', ['server'])
+});
+
+process.on('exit', function () {
+  if (node) { node.kill() }
+})
